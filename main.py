40b6ed48405761db5581bb8cdaa4b278af9ad9c9
benroshan100 <benroshan100@gmail.com>
2024-07-08 22:57:50 +0530

    Added Prediction to frontend

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from wsgiref import simple_server
 from flask import Flask, request, render_template, Response
 import os
+import pandas as pd
 from flask_cors import CORS, cross_origin
 import flask_monitoringdashboard as dashboard
 import json
@@ -13,89 +14,63 @@ app = Flask(__name__)
 dashboard.bind(app)
 CORS(app)
 
-
 @app.route("/", methods=["GET", "POST"])
 @cross_origin()
 def home():
     if request.method == "POST":
-        data = request.form
-        sales_country = data.get("sales_country")
-        n_products = data.get("n_products")
-        promotion_name = data.get("promotion_name")
-        
-        store_ids = {f"store_id_{i}": data.get(f"store_id_{i}", "0") for i in range(20)}
-        
-        promotion_methods = {
-            "Daily_Paper": data.get("Daily_Paper", "0"),
-            "Radio": data.get("Radio", "0"),
-            "In_Store_Coupon": data.get("In_Store_Coupon", "0"),
-            "Product_Attachment": data.get("Product_Attachment", "0"),
-            "Sunday_Paper": data.get("Sunday_Paper", "0"),
-            "TV": data.get("TV", "0"),
-            "Street_Handout": data.get("Street_Handout", "0"),
-            "Bulk_Mail": data.get("Bulk_Mail", "0"),
-            "Cash_Register_Handout": data.get("Cash_Register_Handout", "0")
-        }
-        
-        print("Sales Country:", sales_country)
-        print("Number of Products:", n_products)
-        print("Promotion Name:", promotion_name)
-        print("Store IDs:", store_ids)
-        print("Promotion Methods:", promotion_methods)
-        
-        return render_template('index.html')
-    
+        try:
+            data = {
+                'sales_country': request.form.get('sales_country'),
+                'n_products': int(request.form.get('n_products')),
+                'promotion_name': request.form.get('promotion_name'),
+                'store_id_0': int(request.form.get('store_id_0', 0)),
+                'store_id_1': int(request.form.get('store_id_1', 0)),
+                'store_id_2': int(request.form.get('store_id_2', 0)),
+                'store_id_3': int(request.form.get('store_id_3', 0)),
+                'store_id_4': int(request.form.get('store_id_4', 0)),
+                'store_id_5': int(request.form.get('store_id_5', 0)),
+                'store_id_6': int(request.form.get('store_id_6', 0)),
+                'store_id_7': int(request.form.get('store_id_7', 0)),
+                'store_id_8': int(request.form.get('store_id_8', 0)),
+                'store_id_9': int(request.form.get('store_id_9', 0)),
+                'store_id_10': int(request.form.get('store_id_10', 0)),
+                'store_id_11': int(request.form.get('store_id_11', 0)),
+                'store_id_12': int(request.form.get('store_id_12', 0)),
+                'store_id_13': int(request.form.get('store_id_13', 0)),
+                'store_id_14': int(request.form.get('store_id_14', 0)),
+                'store_id_15': int(request.form.get('store_id_15', 0)),
+                'store_id_16': int(request.form.get('store_id_16', 0)),
+                'store_id_17': int(request.form.get('store_id_17', 0)),
+                'store_id_18': int(request.form.get('store_id_18', 0)),
+                'store_id_19': int(request.form.get('store_id_19', 0)),
+                'Daily_Paper': int(request.form.get('Daily_Paper', 0)),
+                'Radio': int(request.form.get('Radio', 0)),
+                'In_Store_Coupon': int(request.form.get('In_Store_Coupon', 0)),
+                'Product_Attachment': int(request.form.get('Product_Attachment', 0)),
+                'Sunday_Paper': int(request.form.get('Sunday_Paper', 0)),
+                'TV': int(request.form.get('TV', 0)),
+                'Street_Handout': int(request.form.get('Street_Handout', 0)),
+                'Bulk_Mail': int(request.form.get('Bulk_Mail', 0)),
+                'Cash_Register_Handout': int(request.form.get('Cash_Register_Handout', 0)),
+            }
+
+            df = pd.DataFrame([data])
+            print(df)
+            pred = prediction(df)
+            predicted_result = pred.predictionFromModel()  
+            if not predicted_result.empty:
+                predicted_value = predicted_result.iloc[0, 0]
+                print(predicted_value)
+            return render_template('index.html', prediction=predicted_value)
+        except Exception as e:
+            print(f"Error occurred: {e}")
+            return render_template('index.html', error=str(e))
+
     return render_template('index.html')
-# trainModelObj = trainModel()
-# trainModelObj.trainingModel()
-# path='D:\ACADEMIC\ML Projects\CAC Prediction\Prediction_Data_Files'
-# pred = prediction(path)
-# json_predictions = pred.predictionFromModel()
-
-# @app.route("/train", methods=['POST'])
-# @cross_origin()
-# def trainRouteClient():
-#     try:
-#         if request.json and 'folderPath' in request.json:
-#             path = request.json['folderPath']
-#         else:
-#             return Response("Invalid input format", status=400)
-        
-#         trainModelObj = trainModel()
-#         trainModelObj.trainingModel()
-#         return Response("Training successful")
-#     except ValueError as ve:
-#         return Response("Error Occurred! %s" % ve, status=500)
-#     except KeyError as ke:
-#         return Response("Error Occurred! %s" % ke, status=500)
-#     except Exception as e:
-#         return Response("Error Occurred! %s" % e, status=500)
-
-# @app.route("/predict", methods=["POST"])
-# @cross_origin()
-# def predictRouteClient():
-#     try:
-#         if request.json is not None:
-#             path = request.json['filepath']
-#         elif request.form is not None:
-#             path = request.form['filepath']
-#         else:
-#             return Response("Invalid input format", status=400)
-        
-#         pred_val = pred_validation(path)
-#         pred_val.prediction_validation()
-#         pred = prediction(path)
-#         path, json_predictions = pred.predictionFromModel()
-#         return Response("Prediction File created at: " + str(path) + ' and few of the predictions are: ' + str(json.loads(json_predictions)))
-#     except ValueError as ve:
-#         return Response("Error Occurred! %s" % ve, status=500)
-#     except KeyError as ke:
-#         return Response("Error Occurred! %s" % ke, status=500)
-#     except Exception as e:
-#         return Response("Error Occurred! %s" % e, status=500)
 
 port = int(os.getenv("PORT", 5000))
 if __name__ == "__main__":
+    app.run(debug=True)
     host = '0.0.0.0'
     httpd = simple_server.make_server(host, port, app)
     httpd.serve_forever()
--- predictFromModel.py
+++ predictFromModel.py
@@ -5,21 +5,18 @@ from application_logging import logger
 from data_ingestion import data_loader
 
 class prediction:
-    def __init__(self,path):
+    def __init__(self,df):
         self.file_object=open("Prediction_Logs/Prediction_Log.txt",'a+')
         self.log_writer=logger.App_Logger()
-        if path is not None:
-            self.file_op=file_methods.File_Operation(self.file_object,self.log_writer)
+        self.raw_data=df
+        self.file_op=file_methods.File_Operation(self.file_object,self.log_writer)
     
     def predictionFromModel(self):
         try:
-            self.file_op.deletePredictionFile()
             self.log_writer.log(self.file_object,'Start of Prediction')
-            data_getter=data_loader.Data_Getter(self.file_object,self.log_writer)
-            data=data_getter.get_data_predict()
             preprocessor=preprocessing.Preprocessor(self.file_object,self.log_writer)
             onehot_encode_cols=['promotion_name','sales_country']
-            data=preprocessor.encode_data_prediction(data,onehot_encode_cols)
+            data=preprocessor.encode_data_prediction(self.raw_data,onehot_encode_cols)
             is_null_present=preprocessor.is_null_present(data)
             if(is_null_present):
                 data=preprocessor.impute_missing_values(data)
@@ -31,6 +28,6 @@ class prediction:
         except Exception as e:
             self.log_writer.log(self.file_object,'Error occurred while running prediction. Error:: %s' %e)
             raise e 
-        return result.head().to_json(orient="records")
+        return result
 
 
